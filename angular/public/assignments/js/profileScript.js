ff55f7174332c0c6ae5ea7e07096d456b2c4a7de
Rose DeMaio <io302rd@gold.ac.uk>
2015-03-31 12:56:05 -0400

    update remove and add comments

--- angular/public/assignments/js/profileScript.js
+++ angular/public/assignments/js/profileScript.js
@@ -3,17 +3,34 @@ var app = angular.module("OnlineUniversity", []);
 app.controller("OnlineUniversityController", 
 function ($scope, $http)
 {
+    // fetch courses for initial pageload
+    $http.get("/api/course")
+    .success( function(res) {
+        $scope.courses = res;
+    });
 
-    $scope.remove = function(index)
+    $scope.openRemoveModal = function(index)
     {
-        $http.delete("/api/course/" + index)
-        .success(function(response) {
-            $scope.courses = response;
+        $scope.removeIndex = index
+        $scope.remove = $scope.remove
+        $scope.courseToRemove = $scope.courses[index]
+        $("#removeModal").modal('show')
+    }
+
+    // hit delete endpoint to remove a course
+    $scope.remove = function()
+    {
+        $http.delete("/api/course/" + $scope.removeIndex)
+        .success(function(res) {
+            $scope.courses = res;
+            $("#removeModal").modal('hide')
         });
     }
 
+    // prepare course to open "edit" modal, show modal
     $scope.openUpdateCourseModal = function(index)
     {
+        // since tihs is update, assign updateCourse to createOrUpdate button
         $scope.createOrUpdate = $scope.updateCourse
         $scope.editingIndex = index
         $scope.courseToCreateOrUpdate = $scope.courses[index]
@@ -21,6 +38,7 @@ function ($scope, $http)
         $("#courseModal").modal('show')
     }
 
+    // hit PUT endpoint to update existing course, close modal
     $scope.updateCourse = function(updateCourse)
     {
         $http.put("/api/course/" + $scope.editingIndex, updateCourse)
@@ -30,17 +48,20 @@ function ($scope, $http)
         })
     }
 
+    // prepare new course to create, show modal
     $scope.openNewCourseModal = function()
     {
-
+        // since this is create, assign createNewCourse to createOrUpdate button
         $scope.createOrUpdate = $scope.createNewCourse;
         $scope.courseForm.$setPristine();
+        // initialize javascript Date for bootstrap modal (otherwise it will blow up)
         var today = new Date()
         $scope.courseToCreateOrUpdate = {name: "", category:"", dateCreated: today, description:""}
         $("#courseModal").modal('show')
         
     }
 
+    // hit POST endpoint to create new course, hide modal
     $scope.createNewCourse = function(newCourse)
     {
         $http.post("/api/course", newCourse)
@@ -50,16 +71,12 @@ function ($scope, $http)
         })
     }
 
+    // format a readable date for the table, ex "1/2/2015"
     $scope.formatReadableDate = function(date)
     {
         var date = new Date(date)
         return  (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear()
     }
 
-    // fetch courses
-    $http.get("/api/course")
-    .success( function(res) {
-        $scope.courses = res;
-    });
 });
 
